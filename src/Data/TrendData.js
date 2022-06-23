f10395bd33e71cf053434942595b0b096f0c2584
Su <su@Sus-MacBook-Pro.local>
2022-06-23 09:48:58 +0700

    End of RightSide

---
+++ src/Data/TrendData.js
@@ -0,0 +1,8 @@
+export const TrendData = [
+    {name: "Minions", share: 97},
+    {name: "Avengers", share: 80.5},
+    {name: "Zainkeepscode", share: 75.5},
+    {name: "Reactjs", share: 72},
+    {name: "Elon Musk", share: 71.9},
+    {name: "Need for Speed", share: 20}
+]
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import "./Home.css"
 import ProfileSide from '../../components/ProfileSide/ProfileSide'
 import PostSide from '../../components/PostSide/PostSide'
+import RightSide from '../../components/RightSide/RightSide'
 
 const Home = () => {
   return (
     <div className="Home">
-        {/* <div className="profileSile">Profile</div> */}
         <ProfileSide/>
-        {/* <div className="postSide">Post</div> */}
         <PostSide/>
-        <div className="rightSide">Right</div>
+        <RightSide/>
     </div>
   )
 }
