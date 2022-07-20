667338fbc4c3731b07646678a0d7f37c54ff29f7
cotnals99 <su.chae@icloud.com>
2022-07-20 11:02:10 +0700

    Routes-Auth-Model&Controller

--- Server/Routes/AuthRoute.js
+++ Server/Routes/AuthRoute.js
@@ -1,10 +1,13 @@
 import express from "express";
+import { registerUser } from "../Controllers/AuthController.js";
 
 const router = express.Router()
 
 router.get('/', async(req, res)=>{
     res.send("Auth router")
 })
+
+router.post('/register', registerUser)
 
 
 export default router
--- Server/index.js
+++ Server/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 dotenv.config();
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5001;
 
 mongoose
   .connect(process.env.MONGO_DB, {
