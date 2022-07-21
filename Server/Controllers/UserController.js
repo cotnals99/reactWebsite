764654370646adf324feb9853c4e86ec718275e9
cotnals99 <su.chae@icloud.com>
2022-07-21 14:04:36 +0700

    Follow&Unfollow user functions

--- Server/Controllers/UserController.js
+++ Server/Controllers/UserController.js
@@ -84,3 +84,56 @@ export const deleteUser = async (req, res) => {
 //         res.status(500).json(error.message)
 //     }
 // }
+
+
+/// Follow a User
+export const followUser = async (req, res) => {
+  const id = req.params.id;
+
+  const { currentUserId } = req.body;
+
+  if (currentUserId === id) {
+    res.status(403).json("Action forbidden");
+  } else {
+    try {
+      const followUser = await UserModel.findById(id);
+      const followingUser = await UserModel.findById(currentUserId);
+
+      if (!followUser.followers.includes(currentUserId)) {
+        await followUser.updateOne({ $push: { followers: currentUserId } });
+        await followingUser.updateOne({ $push: { following: id } });
+        res.status(200).json("User followed!");
+      } else {
+        res.status(403).json("User is Already followed by you");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+};
+
+// UnFollow a User
+export const unFollowUser = async (req, res) => {
+  const id = req.params.id;
+
+  const { currentUserId } = req.body;
+
+  if (currentUserId === id) {
+    res.status(403).json("Action forbidden");
+  } else {
+    try {
+      const followUser = await UserModel.findById(id);
+      const followingUser = await UserModel.findById(currentUserId);
+
+      if (followUser.followers.includes(currentUserId)) {
+        await followUser.updateOne({ $pull: { followers: currentUserId } });
+        await followingUser.updateOne({ $pull: { following: id } });
+        res.status(200).json("User Unfollowed!");
+      } else {
+        res.status(403).json("User is not followed by you");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+};
--- Server/Routes/UserRoute.js
+++ Server/Routes/UserRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import {getUser, updateUser, deleteUser} from '../Controllers/UserController.js'
+import {getUser, updateUser, deleteUser, followUser, unFollowUser} from '../Controllers/UserController.js'
 
 const router = express.Router()
 
 router.get('/:id', getUser)
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
+router.put('/:id/follow', followUser)
+router.put('/:id/unfollow', unFollowUser)
 
 export default router
